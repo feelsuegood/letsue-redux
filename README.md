72a26dd1e5927534dc9d43e4fdda4f43808dab5c
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-17 17:59:36 +1000

    vanilla JS with redux

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Redux
+
+## References
+- [Redux 101 for Beginners by Nomad Coders (Mar 2020)](https://nomadcoders.co/redux-for-beginners)
--- src/index.js
+++ src/index.js
@@ -1,22 +1,75 @@
+import { legacy_createStore as createStore } from "redux";
+
 const increment = document.getElementById("increment");
 const decrement = document.getElementById("decrement");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
+/**
+ * old way
+ */
+// let count = 0;
+// number.innerText = count;
 
-const updateText = () => {
-  number.innerText = count;
-};
-const handleIncrement = () => {
-  count++;
-  updateText();
+// const updateText = () => {
+//   number.innerText = count;
+// };
+// const handleIncrement = () => {
+//   count++;
+//   updateText();
+// };
+
+// const handleDecrement = () => {
+//   count--;
+//   updateText();
+// };
+
+// increment.addEventListener("click", handleIncrement);
+// decrement.addEventListener("click", handleDecrement);
+
+number.innerText = 0;
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+
+const countModifier = (count = 0, action) => {
+  // console.log(count, action);
+  // if (action.type === "INCREMENT") {
+  //   return count + 1;
+  // } else if (action.type === "DECREMENT") {
+  //   return count - 1;
+  // } else {
+  //   return count;
+  // }
+  switch (action.type) {
+    case INCREMENT:
+      return count + 1;
+    case DECREMENT:
+      return count - 1;
+    default:
+      return count;
+  }
 };
 
-const handleDecrement = () => {
-  count--;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-increment.addEventListener("click", handleIncrement);
-decrement.addEventListener("click", handleDecrement);
+const onChange = () => {
+  // console.log(countStore.getState());
+  number.innerText = countStore.getState();
+};
+countStore.subscribe(onChange);
+
+// countStore.dispatch({ type: "INCREMENT" });
+// countStore.dispatch({ type: "INCREMENT" });
+// countStore.dispatch({ type: "INCREMENT" });
+// countStore.dispatch({ type: "INCREMENT" });
+// countStore.dispatch({ type: "INCREMENT" });
+// countStore.dispatch({ type: "DECREMENT" });
+
+// console.log(countStore);
+// console.log(countStore.getState());
+
+increment.addEventListener("click", () => {
+  countStore.dispatch({ type: INCREMENT });
+});
+decrement.addEventListener("click", () => {
+  countStore.dispatch({ type: DECREMENT });
+});
