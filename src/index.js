02736b41de47046d0194622aef2f023d53b61752
feelsuegood <129813530+feelsuegood@users.noreply.github.com>
2025-06-17 17:15:08 +1000

    vanilla ver

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const increment = document.getElementById("increment");
+const decrement = document.getElementById("decrement");
+const number = document.querySelector("span");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
+number.innerText = count;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const updateText = () => {
+  number.innerText = count;
+};
+const handleIncrement = () => {
+  count++;
+  updateText();
+};
+
+const handleDecrement = () => {
+  count--;
+  updateText();
+};
+
+increment.addEventListener("click", handleIncrement);
+decrement.addEventListener("click", handleDecrement);
